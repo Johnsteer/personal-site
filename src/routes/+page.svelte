55d3f2a5ca41088bfc6f2efdd849fb169243bf2f
John Steer <johnsteer3@gmail.com>
2024-09-03 20:57:33 -0400

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+    import Nav from "../components/Nav.svelte";
+</script>
+
+<div class="wrapper">
+    <div class="container">
+        <h1>John Steer</h1>
+        <Nav />
+        <p>
+            I've recently been binging content from Fireship and ThePrimeTime and
+            it made me want to learn how to build web apps so here I am starting
+            a journey building my personal site. Forcing myself to write everything
+            myself and not use Cursor/Claude so I actually learn things. I tried React
+            but idc about job market I think Svelte is cooler so I'm using it.
+            I WILL FINISH THIS PROJECT AND DEPLOY IT ON GOOGLE CLOUD RUN!!!!!!!!!!!!! Also doing all the html/css from
+            scratch cause it's more fun than having AI generate it. Following along the Svelte 
+            interactive tutorial to figure stuff out.
+        </p>
+    </div>
+</div>
+
+<style>
+    .wrapper {
+        padding: 0 10%;
+    }
+
+    .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
+

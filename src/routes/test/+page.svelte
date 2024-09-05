b88647044a29fc3757ec0db9940702277217ae1b
John Steer <johnsteer3@gmail.com>
2024-09-04 22:15:32 -0400

    started reading feed

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,64 @@
+<script>
+    let admin = true;
+    let posts = [];
+</script>
+
+{#if admin}
+    <div style="display: flex;">
+        <div>
+            {#if posts.length === 0}
+                <h1>No Posts</h1>
+            {:else}
+            {#each posts as post}
+                <div class="item">
+                    <a href={post.link}>{post.title}</a>
+                    <p>{post.author}</p>
+                </div> 
+            {/each}
+            {/if}        
+        </div>
+        <div>
+            <form>
+                <div class="vflex">
+                    <div class="item">
+                        <label for="title">Title:</label>
+                        <input id="title" type="text">
+                    </div>
+                    <div class="item">
+                        <label for="author">Author</label>
+                        <input id="author" type="text">
+                    </div>
+                    <div class="item">
+                        <label for="link">Link</label>
+                        <input id="link" type="text">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+{:else}
+    <div style="display: flex;">
+        {#if posts.length === 0}
+            <h1>No Posts</h1>
+        {:else}
+        {#each posts as post}
+            <div class="item">
+                <a href={post.link}>{post.title}</a>
+                <p>{post.author}</p>
+            </div> 
+        {/each}
+        {/if}        
+    </div>
+{/if}
+
+<style>
+    .vflex {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .item {
+        display: flex;
+        flex: 1;
+    }
+</style>

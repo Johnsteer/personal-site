027529450a3ef37b3be70548259686c839501585
John Steer <johnsteer3@gmail.com>
2024-09-09 21:44:04 -0400

    set up pocketbase and test JS SDK

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,36 @@
     import Nav from "../components/Nav.svelte";
     import Blog from "../components/Blog.svelte";
     import { g_selectedItem } from "../components/store";
+    import PocketBase from "pocketbase";
+    import { onMount, onDestroy } from "svelte";
 
+    // initialize empty posts array
+    let posts = [];
+
+    // global variable subscription
     $: selectedItem = $g_selectedItem;
+
+    // https://github.com/pocketbase/js-sdk?tab=readme-ov-file#installation
+    // create pocketbase connection object
+    // default pocketbase setup with "posts" collection with fields (title, content, user)
+    // test recorded created manually with admin UI
+    const pb = new PocketBase("http://127.0.0.1:8090");
+
+    // run once on mount (refresh)
+    onMount(async () => {
+        console.log("mounting...");
+        // attempt to pull posts collection data - make sure to grant appropriate endpoint permissions
+        try {
+            const resultList = await pb.collection("posts").getList(1, 50, {
+            sort: "created", // return 1 page up to 50 items sorted by creation time
+            });
+            posts = resultList.items;
+            console.log(posts);  
+
+        } catch (error) {
+            console.log(error); //log error
+        }
+    });
 
 </script>
 
@@ -26,7 +54,10 @@
         {:else if selectedItem === 1}
             <Blog />
         {:else}
-            Stuff I'm building        
+            <!-- Render content field value for returned posts-->
+            {#each posts as post (post.id)}
+                {post.content}
+            {/each}      
         {/if}
 
     </div>
